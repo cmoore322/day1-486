eab1e68f7d4558af698eb9ae48602e164ab38226
cmoore322 <cmoore32@una.edu>
2025-09-04 13:36:21 -0500

    mongo code needs to be fixed :(

--- app.mjs
+++ app.mjs
@@ -13,6 +13,33 @@ app.use(express.static(join(__dirname, 'public')));
 app.use(express.json()); 
 
 
+const uri = "mongodb+srv://cmoore32_db_user:guslebronmoore@cluster0.nmc10zq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello Express from Render 😍😍😍. <a href="connor">connor</a>')
@@ -23,7 +50,7 @@ app.get('/', (req, res) => {
 // send an html file
 app.get('/connor', (req, res) => {
  
-  res.sendFile(join(__dirname, 'public', 'barry.html')) 
+  res.sendFile(join(__dirname, 'public', 'connor.html')) 
 
 })
 
